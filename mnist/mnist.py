971de2f0234684e41c2436cc35b87d2c2fbfab47
hranjbar <horanj.1022@gmail.com>
2023-06-08 22:50:40 -0400

    Added scripts to test TF C API and cppflow

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -32,7 +32,7 @@ model.compile(
         metrics=['accuracy']
 )
 
-history = model.fit(x_train, y_train, epochs=10, batch_size=32)
+history = model.fit(x_train, y_train, epochs=5, batch_size=32)
 
 image = random.choice(x_test)
 
